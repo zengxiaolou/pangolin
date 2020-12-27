c374b1afde566751bdac913bc9a4f4a010d4cdc2
Shelton Suen <sheltonsuen@gmail.com>
2020-12-27 14:18:11 +0800

    feat(#1): pull latest image instead of local cache

--- deploy.sh
+++ deploy.sh
@@ -2,5 +2,6 @@
 
 # run vpn container
 echo "run vpn container"
+sudo docker pull $CI_REGISTRY_IMAGE:latest
 sudo docker container rm -f vpn-instance
 sudo docker run --name vpn-instance -d -p 1544:1544 $CI_REGISTRY_IMAGE:latest
