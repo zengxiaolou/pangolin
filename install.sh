4ae355fe43db54b86d5578cf457495459a9b396e
Shelton Suen <sheltonsuen@gmail.com>
2020-12-27 11:27:17 +0800

    feat(#1): run vpn instance on 1544

--- install.sh
+++ install.sh
@@ -29,3 +29,9 @@ else
     echo "start docker"
     sudo systemctl start docker
 fi
+
+
+# run vpn container
+echo "run vpn container"
+sudo docker container rm -f vpn-instance
+sudo docker run --name vpn-instance -d -p 1544:1544 $CI_REGISTRY_IMAGE:latest
