82623d1ae57bd5480ba68aa7da27c8f94f17a20d
Shelton Suen <sheltonsuen@deepstudy.tech>
2021-06-23 21:34:16 +0800

    feat(#2): update port

--- deploy_outside.sh
+++ deploy_outside.sh
@@ -4,4 +4,4 @@
 echo "run vpn container"
 sudo docker pull $CI_REGISTRY_IMAGE/outside:latest
 sudo docker container rm -f vpn-outside-instance
-sudo docker run --name vpn-outside-instance -d -p 1588:1588 $CI_REGISTRY_IMAGE/outside:latest
+sudo docker run --name vpn-outside-instance -d -p 1589:1589 $CI_REGISTRY_IMAGE/outside:latest
