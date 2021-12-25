784fa4a1ed5649ca0da2d3a7a2d0d08b57e8dedb
Shelton Suen <sheltonsuen@deepstudy.tech>
2021-12-25 12:02:58 +0800

    remove unused

--- deploy_outside.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# run vpn container
-echo "run vpn container"
-sudo docker pull $CI_REGISTRY_IMAGE/outside:latest
-sudo docker container rm -f vpn-outside-instance
-sudo docker run --name vpn-outside-instance -d -p 1588:1588 $CI_REGISTRY_IMAGE/outside:latest
